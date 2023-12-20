fa000426f49fa33c14b20da6e044aacf2b098b26
Ivan-Galenko <154437118+Ivan-Galenko@users.noreply.github.com>
2023-12-20 17:02:45 +0200

    Task #3
    Ülesanne "Funktsioonide print(),input(), float(), int() kasutamine"

---
+++ IG_0.3.py
@@ -0,0 +1,10 @@
+from random import *
+kogus = randint(1,100)
+print('Kokku on ', kogus, ' kommid')
+mitu = int(input('Mitu kommi tahad votta?'))
+jaak = kogus - mitu
+print("mul on ", jaak)
+#if jaak > 0:
+#  print("mul on ", jaak)
+#else:
+#  print('ma ei saa nii palju anda')
